2cf672d8dce65dffa82edb989415635c87c7d42b
Anas Ahmed <anasahmed700@gmail.com>
2024-09-05 18:29:48 +0500

    Component Composition: The special children Prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
 import { CORE_CONCEPTS } from "./assets/data";
-import reactImg from "./assets/react-core-concepts.png";
+import CoreConcept from "./components/CoreConcept";
+import Header from "./components/Header";
+import TabButton from "./components/TabButton";
 
-const reactDesc = ["Fundamental", "Crucial", "Core"];
-
-function genRandInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDesc[genRandInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreConcept({ title, description, image }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
 function App() {
   return (
     <div>
@@ -47,7 +20,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>States</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
