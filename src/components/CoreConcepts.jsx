909ba807050680132001a641b2ec79bfd1dd64a0
Anas Ahmed <anasahmed700@gmail.com>
2024-10-21 15:11:08 +0500

    Splitting Components By Feature & State

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { CORE_CONCEPTS } from '../assets/data'
+import CoreConcept from './CoreConcept'
+
+const CoreConcepts = () => {
+    return (
+        <section id="core-concepts">
+            <ul>
+                {CORE_CONCEPTS.map(conceptItem => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
+            </ul>
+        </section>
+    )
+}
+
+export default CoreConcepts
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,5 +1,4 @@
 const TabButton = ({ children, onSelect, isSelected }) => {
-    console.log("Tab Component Executing...")
     return (
         <li>
             <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
